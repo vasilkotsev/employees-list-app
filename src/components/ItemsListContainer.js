70841a98ba91f2ec9faaf6d46e4b0b2ccee26e80
Vasil Kotsev <vasil.kotsev@gmail.com>
2020-10-15 21:36:07 +0300

    removes some comments

--- src/components/ItemsListContainer.js
+++ src/components/ItemsListContainer.js
@@ -47,9 +47,6 @@ class ItemsListContainer extends React.Component {
             items: result,
           });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {
           this.setState({
             isLoaded: true,
